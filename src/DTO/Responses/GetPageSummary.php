8abe19a03e271a7637086f36e4c932affeb4f171
Denis Kulagin <denis.kulagin1593@mail.ru>
2022-10-09 00:29:43 +0700

    Description of the response structure

--- src/DTO/Responses/GetPageSummary.php
+++ src/DTO/Responses/GetPageSummary.php
@@ -2,7 +2,29 @@
 
 namespace MediawikiSdkPhp\DTO\Responses;
 
-class GetPageSummary
-{
+use Spatie\DataTransferObject\DataTransferObject;
 
+class GetPageSummary extends DataTransferObject
+{
+    public array $titles;
+
+    public int $pageid;
+
+    public string $extract;
+
+    public string $extract_html;
+
+    public array $thumbnail;
+
+    public array $originalimage;
+
+    public string $lang;
+
+    public string $dir;
+
+    public string $timestamp;
+
+    public string $description;
+
+    public array $coordinates;
 }
